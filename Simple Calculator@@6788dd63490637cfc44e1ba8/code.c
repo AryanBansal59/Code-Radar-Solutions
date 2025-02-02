292f0a64f14d88fc0d0cb1d5ea8c7c9a40af5c0e
AryanBansal59 <3073aryanbansal.davk@gmail.com>
2025-02-02 21:01:05 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -3,19 +3,19 @@
 int main() {
     int a,b,c;char oper[100];
     scanf("%d %d %s",&a,&b,oper);
-    if (strcmp(oper == '+')==0){
+    if (strcmp(oper,'+')==0){
         c = a+b;
         printf("%d",c);
     }
-    else if (strcmp(oper == '-')==0){
+    else if (strcmp(oper, '-')==0){
         c = a-b;
         printf("%d",c);
     }
-    else if (strcmp(oper == '*')==0){
+    else if (strcmp(oper,'*')==0){
         c = a*b;
         printf("%d",c);
     }
-    else if(strcmp(oper == '/')==0){
+    else if(strcmp(oper,'/')==0){
         if (b!=0){
             c = a/b;
             printf("%d",c)
