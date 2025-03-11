fedfdb4cba86b4bd9e23918eb7c2ea2df67e0e70
Aryan Bansal <3073aryanbansal.davk@gmail.com>
2025-03-11 18:42:50 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -5,5 +5,5 @@ int fibonacciSeries(int n){
     if (n == 1){
         return 1;
     }
-        return fibonacciSeries(n-1) + fibonacciSeries(n-2);
+    return fibonacciSeries(n-1) + fibonacciSeries(n-2);
 }
