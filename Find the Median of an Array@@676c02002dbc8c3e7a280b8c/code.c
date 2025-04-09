8bfd71a16f6a8a70f847a30dbbe7e59d1ea5c597
Aryan Bansal <3073aryanbansal.davk@gmail.com>
2025-04-09 17:13:48 +0530

    Solution to Find the Median of an Array

--- Find the Median of an Array@@676c02002dbc8c3e7a280b8c/code.c
+++ Find the Median of an Array@@676c02002dbc8c3e7a280b8c/code.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 int main(){
-    int a;
+    int a,c;
     scanf("%d",&a);
     int arr[100];
     for (int i = 0; i < a; i++){
         scanf("%d",&arr[i]);
     }
     if (a % 2 == 0){
-        int c = (arr[a/2] + arr[a/2 + 1])/2;
+        c = (arr[a/2] + arr[a/2 + 1])/2;
     }
     printf("%d",c);
 }
